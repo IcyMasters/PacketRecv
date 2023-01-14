091d9af5ef32d3dfca758e7360f740aab9a2cdb3
ZenMaxe <Sanjabian.ho@gmail.com>
2023-01-15 00:03:01 +0330

    change Field of struct to readonly add struct and enum for Packetrecv

--- PacketRecv.Main/NetStat.cs
+++ PacketRecv.Main/NetStat.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +24,7 @@ namespace PacketRecv.Main
         SYN_SENT,
         TIMED_WAIT,
         None, // UDP
+        Empty,
     }
 
     public enum ConnectionType
@@ -28,16 +32,165 @@ namespace PacketRecv.Main
         TCP,
         UDP,
         Other,
+        Empty,
     }
+
     public struct NetStat
     {
-        public ConnectionType CType;
-        public IPAddress LAddress;
-        public IPAddress FAddress;
-        public State State;
-        public int Pid;
+        public readonly ConnectionType CType;
+        public readonly IPAddress LAddress;
+        public readonly IPAddress FAddress;
+        public readonly State State;
+        public readonly int Pid;
 
-        public IPAddress iPAddress(string ip) => IPAddress.Parse(ip);
+        public NetStat(ConnectionType ct = ConnectionType.Empty,
+            IPAddress la = null,
+            IPAddress fa = null,
+            State st = State.Empty,
+            int pid = 0)
+        {
+            this.CType = ct;
+            this.LAddress = la;
+            this.FAddress = fa;
+            this.State = st;
+            this.Pid = pid;
+        }
+    }
 
+    
+    
+    // PacketRecv.cs 
+    public class TcpProcessRecord
+    {
+        [DisplayName("Local Address")]
+        public IPAddress LocalAddress { get; set; }
+        
+        [DisplayName("Local Port")]
+        public ushort LocalPort { get; set; }
+        
+        [DisplayName("Remote Address")]
+        public IPAddress RemoteAddress { get; set; }
+        
+        [DisplayName("Remote Port")]
+        public ushort RemotePort { get; set; }
+        
+        [DisplayName("State")]
+        public MibTcpState State { get; set; }
+        
+        [DisplayName("Process ID")]
+        public int ProcessId { get; set; }
+        
+        [DisplayName("Process Name")]
+        public string ProcessName { get; set; }
+
+        public TcpProcessRecord(IPAddress localIp, IPAddress remoteIp, ushort localPort,
+            ushort remotePort, int pId, MibTcpState state)
+        {
+            LocalAddress = localIp;
+            RemoteAddress = remoteIp;
+            LocalPort = localPort;
+            RemotePort = remotePort;
+            State = state;
+            ProcessId = pId;
+            // Getting the process name associated with a process id.
+            if (Process.GetProcesses().Any(process => process.Id == pId))
+            {
+                ProcessName = Process.GetProcessById(ProcessId).ProcessName;
+            }
+        }
+    }
+
+
+    
+
+    public struct MIB_TCPROW_OWNER_PID
+    {
+        public MibTcpState state;
+        public uint localAddr;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] localPort;
+        public uint remoteAddr;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] remotePort;
+        public int owningPid;
+    }
+
+    public struct MIB_TCPTABLE_OWNER_PID
+    {
+        public uint dwNumEntries;
+        [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct,
+            SizeConst = 1)]
+        public MIB_TCPROW_OWNER_PID[] table;
+    }
+
+    public enum TcpTableClass
+    {
+        TCP_TABLE_BASIC_LISTENER,
+        TCP_TABLE_BASIC_CONNECTIONS,
+        TCP_TABLE_BASIC_ALL,
+        TCP_TABLE_OWNER_PID_LISTENER,
+        TCP_TABLE_OWNER_PID_CONNECTIONS,
+        TCP_TABLE_OWNER_PID_ALL,
+        TCP_TABLE_OWNER_MODULE_LISTENER,
+        TCP_TABLE_OWNER_MODULE_CONNECTIONS,
+        TCP_TABLE_OWNER_MODULE_ALL
+    }
+
+    public enum UdpTableClass
+    {
+        UDP_TABLE_BASIC,
+        UDP_TABLE_OWNER_PID,
+        UDP_TABLE_OWNER_MODULE
+    }
+
+    public enum MibTcpState
+    {
+        CLOSED = 1,
+        LISTENING = 2,
+        SYN_SENT = 3,
+        SYN_RCVD = 4,
+        ESTABLISHED = 5,
+        FIN_WAIT1 = 6,
+        FIN_WAIT2 = 7,
+        CLOSE_WAIT = 8,
+        CLOSING = 9,
+        LAST_ACK = 10,
+        TIME_WAIT = 11,
+        DELETE_TCB = 12,
+        NONE = 0
+    }
+    public struct MIB_UDPROW_OWNER_PID
+    {
+        public uint localAddr;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] localPort;
+        public int owningPid;
+    }
+    public struct MIB_UDPTABLE_OWNER_PID
+    {
+        public uint dwNumEntries;
+        [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct,
+            SizeConst = 1)]
+        public UdpProcessRecord[] table;
+    }
+    public class UdpProcessRecord
+    {
+        [DisplayName("Local Address")]
+        public IPAddress LocalAddress { get; set; }
+        [DisplayName("Local Port")]
+        public uint LocalPort { get; set; }
+        [DisplayName("Process ID")]
+        public int ProcessId { get; set; }
+        [DisplayName("Process Name")]
+        public string ProcessName { get; set; }
+
+        public UdpProcessRecord(IPAddress localAddress, uint localPort, int pId)
+        {
+            LocalAddress = localAddress;
+            LocalPort = localPort;
+            ProcessId = pId;
+            if (Process.GetProcesses().Any(process => process.Id == pId))
+                ProcessName = Process.GetProcessById(ProcessId).ProcessName;
+        }
     }
 }
